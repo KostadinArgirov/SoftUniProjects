efb71dd0006c2288d3e7ca99a0158c6277d7f6a0
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-05-23 11:21:23 +0300

    New problem solving solution added

--- JavaAdvanced/src/stacksAndQueuesExercise/P06RecursiveFibonacci.java
+++ JavaAdvanced/src/stacksAndQueuesExercise/P06RecursiveFibonacci.java
@@ -1,32 +1,56 @@
 package stacksAndQueuesExercise;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class P06RecursiveFibonacci {
     public static long[] memory;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+//        int n = Integer.parseInt(scanner.nextLine());
+//
+//        memory = new long[n + 1];
+//
+//        if (n == 0) {
+//            System.out.println("0");
+//            return;
+//        }
+//
+//        long result = fib(n);
+//        System.out.println(result);
+//    }
+//    private static long fib(int n) {
+//        if (n < 2) {
+//            return 1;
+//        }
+//        if (memory[n] != 0) {
+//            return memory[n];
+//        }
+//        memory[n] = fib(n - 1) + fib(n - 2);
+//        return memory[n];
+//    }
+//}
+
         int n = Integer.parseInt(scanner.nextLine());
 
-        memory = new long[n + 1];
-
-        if (n == 0) {
-            System.out.println("0");
+        if (n < 2) {
+            System.out.println("1");
             return;
         }
+        ArrayDeque<Long> stack = new ArrayDeque<>();
 
-        long result = fib(n);
-        System.out.println(result);
-    }
-    private static long fib(int n) {
-        if (n < 2) {
-            return 1;
+        stack.push(0l);
+        stack.push(1l);
+
+        for (int i = 1; i <= n; i++) {
+            long num1 = stack.pop();
+            long num2 = stack.pop();
+
+            stack.push(num1);
+            stack.push(num1 + num2);
         }
-        if (memory[n] != 0) {
-            return memory[n];
-        }
-        memory[n] = fib(n - 1) + fib(n - 2);
-        return memory[n];
+        System.out.println(stack.pop());
     }
 }
