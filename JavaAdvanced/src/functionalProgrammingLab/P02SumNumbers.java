b9732d6a058f716a128980f0fa6349788e33084c
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-05 23:56:15 +0300

    New Project Class Added

---
+++ JavaAdvanced/src/functionalProgrammingLab/P02SumNumbers.java
@@ -0,0 +1,29 @@
+package functionalProgrammingLab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class P02SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Function<List<Integer>, String> countFormatter = list -> "Count = " + list.size();
+        Function<List<Integer>, Integer> sumAllElements = l -> l.stream().mapToInt(e -> e).sum();
+        Function<Integer, String> sumFormatter = s -> "Sum = " + s;
+
+        String countOutput = countFormatter.apply(numbers);
+
+        System.out.println(countOutput);
+
+        int sum = sumAllElements.apply(numbers);
+        String sumOutput = sumFormatter.apply(sum);
+        System.out.println(sumOutput);
+    }
+}
