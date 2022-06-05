b5db7ba15fea390148b54038fcf83e5950d188de
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-05 17:18:03 +0300

    New Project Class Added

---
+++ JavaAdvanced/src/streamsFilesAndDirectoriesLab/P07ListFiles.java
@@ -0,0 +1,16 @@
+package streamsFilesAndDirectoriesLab;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class P07ListFiles {
+    public static void main(String[] args) {
+
+        File root = new File("D:\\Argirov\\SoftUni\\Java Advanced May 2022\\Resources\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
+
+        Arrays.stream(root.listFiles())
+                .filter(File::isFile)
+                .forEach(f -> System.out.println(f.getName() + ": " + "[" + f.length() + "]"));
+    }
+}
