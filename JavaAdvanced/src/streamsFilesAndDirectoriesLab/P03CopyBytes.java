95612cb988d2848c48eeca9d3ccc501c40e4f450
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-05 13:03:25 +0300

    New Project Class Added

--- JavaAdvanced/src/streamsFilesAndDirectoriesLab/P03CopyBytes.java
+++ JavaAdvanced/src/streamsFilesAndDirectoriesLab/P03CopyBytes.java
@@ -3,28 +3,33 @@ package streamsFilesAndDirectoriesLab;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Set;
+import java.io.PrintWriter;
 
-public class P02WriteToFile {
+public class P03CopyBytes {
     public static void main(String[] args) {
-
         String path = "D:\\Argirov\\SoftUni\\Java Advanced May 2022\\Resources\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
 
         try {
             FileInputStream fileInputStream = new FileInputStream(path);
-            FileOutputStream fileOutputStream = new FileOutputStream("02WriteToFileOutput.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream("03CopyBytesOutput.txt");
 
             int read = fileInputStream.read();
 
-            Set<Character> punctuations = Set.of(',', '.', '!', '?');
+            PrintWriter writer = new PrintWriter(fileOutputStream);
 
             while (read != -1) {
 
-                if (!punctuations.contains((char) read)) {
-                    fileOutputStream.write(read);
+                if (read == ' ') {
+                    writer.print(' ');
+                } else if(read == 10) {
+                    writer.println();
+                } else {
+                    writer.print(read);
                 }
+
                 read = fileInputStream.read();
             }
+            writer.close();
         } catch (IOException ignored) {
         }
     }
