525fcffc3837fe845e57fe1b686286a92ef2fc0d
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-05 16:27:20 +0300

    New Project Class Added

---
+++ JavaAdvanced/src/streamsFilesAndDirectoriesLab/P05WriteEveryThirdLine.java
@@ -0,0 +1,32 @@
+package streamsFilesAndDirectoriesLab;
+
+import java.io.*;
+
+public class P05WriteEveryThirdLine {
+    public static void main(String[] args) throws IOException {
+
+        String path = "D:\\Argirov\\SoftUni\\Java Advanced May 2022\\Resources\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("05WriteEveryThirdLineOutput.txt"));
+
+        String line = reader.readLine();
+
+        int counter = 0;
+
+        while (line != null) {
+
+            counter++;
+
+            if (counter % 3 == 0) {
+                writer.write(line);
+                writer.newLine();
+            }
+
+            line = reader.readLine();
+        }
+        reader.close();
+        writer.close();
+    }
+}
