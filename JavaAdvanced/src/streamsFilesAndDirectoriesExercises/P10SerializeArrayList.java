88b0f16215feeb48caf7a8ecb831968b24659e4c
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-05 22:14:10 +0300

    New Project Class Added

---
+++ JavaAdvanced/src/streamsFilesAndDirectoriesExercises/P10SerializeArrayList.java
@@ -0,0 +1,27 @@
+package streamsFilesAndDirectoriesExercises;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P10SerializeArrayList {
+    public static void main(String[] args) {
+
+        String path = "src/streamsFilesAndDirectoriesExercises/04. Java-Advanced-Files-and-Streams-Exercises-Resources/list.ser";
+
+        List<String> bikes = new ArrayList<>();
+
+        bikes.add("GSXR1000");
+        bikes.add("R1");
+        bikes.add("CBR1000R");
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+             ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
+            oos.writeObject(bikes);
+            Object listFromFile = ois.readObject();
+            System.out.println(listFromFile);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
