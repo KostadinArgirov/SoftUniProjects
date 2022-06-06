69d24cbd0f3059eb022842e23c928039218c48ee
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-06 11:33:16 +0300

    New Project Class Added

---
+++ JavaAdvanced/src/functionalProgrammingExercises/P03CustomMinFunction.java
@@ -0,0 +1,25 @@
+package functionalProgrammingExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class P03CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        Function<int[], Integer> smallestInt = (int[] arr) -> {
+            int min = Integer.MAX_VALUE;
+
+            for (int element : arr) {
+                if (element < min) {
+                    min = element;
+                }
+            }
+            return min;
+        };
+        System.out.println(smallestInt.apply(numbers));
+    }
+}
