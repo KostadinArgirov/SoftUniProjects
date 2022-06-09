2ab31c2626d8dd0fa5077183a2395e28bc12d8be
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-09 16:07:06 +0300

    New Project Packages And New Classes Added

---
+++ JavaAdvanced/src/definingClassesExercises/Google/Company.java
@@ -0,0 +1,17 @@
+package definingClassesExercises.Google;
+
+public class Company {
+
+    private final String name;
+    private final String department;
+    private final double salary;
+
+    public Company(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+    public String format(){
+        return String.format("%s %s %.2f",name,department,salary);
+    }
+}
