ed3ed3740301a3854aa0a928d433057b6c1212ca
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-09 16:28:37 +0300

    New Project Packages And New Classes Added

---
+++ JavaAdvanced/src/definingClassesExercises/CatLady/StreetExtraordinaire.java
@@ -0,0 +1,16 @@
+package definingClassesExercises.CatLady;
+
+public class StreetExtraordinaire extends Cat{
+
+    private final double decibelsOfMeows;
+
+    public StreetExtraordinaire(String name, String breed, double decibelsOfMeows) {
+        super(name, breed);
+        this.decibelsOfMeows = decibelsOfMeows;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %.2f", getBreed(), getName(), this.decibelsOfMeows);
+    }
+}
