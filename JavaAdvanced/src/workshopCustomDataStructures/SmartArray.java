e8ad40b4e9e24eb2605430fdf023dc82e3695c9f
Kostadin Argirov <argirov.kostadin@gmail.com>
2022-06-17 09:36:10 +0300

    Make Custom Data Structures Iterable

--- JavaAdvanced/src/workshopCustomDataStructures/SmartArray.java
+++ JavaAdvanced/src/workshopCustomDataStructures/SmartArray.java
@@ -1,17 +1,38 @@
 package workshopCustomDataStructures;
 
-import java.util.function.Consumer;
+import java.util.Iterator;
 
-public class SmartArray {
-    private Integer[] data;
+public class SmartArray<E> implements Iterable<E> {
+    private Object[] data;
     private int size;
 
     public SmartArray() {
-        this.data = new Integer[4];
+        this.data = new Object[4];
         this.size = 0;
     }
 
-    public void add(int element) {
+    private class SmartArrayIterator implements Iterator<E> {
+        private int i = 0;
+
+        @Override
+        public boolean hasNext() {
+            return i < size;
+        }
+
+        @Override
+        public E next() {
+            E element = get(i);
+            i++;
+            return element;
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new SmartArrayIterator();
+    }
+
+    public void add(E element) {
         if (this.size == this.data.length) {
             this.data = grow();
         }
@@ -20,26 +41,27 @@ public class SmartArray {
         this.size++;
     }
 
-    public int get(int index) {
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
         validateIndex(index);
 
-        return this.data[index];
+        return (E) this.data[index];
     }
 
     public int size() {
         return this.size;
     }
 
-    public int remove(int index) {
+    public E remove(int index) {
         validateIndex(index);
         this.size--;
-        int element = this.data[index];
+        E element = get(index);
 
         if (this.size - index >= 0) {
             System.arraycopy(this.data, index + 1, this.data, index, this.size - index);
         }
 
-        this.data[this.size] = null;
+        this.data[this.size] = 0;
 
         if (this.data.length / 4 >= this.size || this.data.length / 4 == 4) {
             this.data = shrink();
@@ -51,7 +73,7 @@ public class SmartArray {
     public boolean contains(int element) {
 
         for (int i = 0; i < this.size; i++) {
-            if (this.data[i] == element) {
+            if (this.data[i].equals(element)) {
                 return true;
             }
         }
@@ -59,11 +81,11 @@ public class SmartArray {
         return false;
     }
 
-    public void add(int index, int element) {
+    public void add(int index, E element) {
         validateIndex(index);
 
         int lastIndex = this.size - 1;
-        int lastElement = this.data[lastIndex];
+        E lastElement = get(lastIndex);
 
         if (lastIndex - index >= 0) {
             System.arraycopy(this.data, index, this.data, index + 1, lastIndex - index);
@@ -74,33 +96,25 @@ public class SmartArray {
         add(lastElement);
     }
 
-    public void forEach(Consumer<Integer> consumer) {
-        for (int i = 0; i < this.size; i++) {
-            Integer e = this.data[i];
-            consumer.accept(e);
-        }
-    }
-
     private void validateIndex(int index) {
         if (index < 0 || index >= this.size) {
             throw new IndexOutOfBoundsException("Invalid index " + index);
         }
     }
 
-    private Integer[] grow() {
-        Integer[] newData = new Integer[this.data.length * 2];
+    private Object[] grow() {
+        Object[] newData = new Object[this.data.length * 2];
 
         System.arraycopy(this.data, 0, newData, 0, this.data.length);
 
         return newData;
     }
 
-    private Integer[] shrink() {
-        Integer[] newData = new Integer[this.data.length / 2];
+    private Object[] shrink() {
+        Object[] newData = new Object[this.data.length / 2];
 
         System.arraycopy(this.data, 0, newData, 0, this.size);
 
         return newData;
     }
-
 }
