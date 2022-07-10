d5ca6e5be54010b2de11ec4b6d8a98b3128fd3ff
Kostadin <argirov.kostadin@gmail.com>
2022-07-10 22:32:40 +0300

    New Test Class Added

---
+++ JavaAdvanced/src/oop/inheritanceLab/Test.java
@@ -0,0 +1,14 @@
+package oop.inheritanceLab;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(a + b);
+    }
+}
