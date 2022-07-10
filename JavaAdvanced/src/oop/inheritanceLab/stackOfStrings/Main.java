4681c7b5db7766cd53c21b34779d4788b1efaecf
Kostadin <argirov.kostadin@gmail.com>
2022-07-10 23:51:36 +0300

    New Project Packages and Classes Added

---
+++ JavaAdvanced/src/oop/inheritanceLab/stackOfStrings/Main.java
@@ -0,0 +1,11 @@
+package oop.inheritanceLab.stackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StackOfStrings strings = new StackOfStrings();
+
+        strings.push("my-solution");
+        System.out.println(strings.pop());
+    }
+}
