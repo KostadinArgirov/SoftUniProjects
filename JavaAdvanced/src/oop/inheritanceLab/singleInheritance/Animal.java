668fd4dd9c7e4a80e58d0d1b64a07d3d7c89984a
Kostadin <argirov.kostadin@gmail.com>
2022-07-10 23:05:38 +0300

    New Project Packages and Classes Added

---
+++ JavaAdvanced/src/oop/inheritanceLab/singleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package oop.inheritanceLab.singleInheritance;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ JavaAdvanced/src/oop/inheritanceLab/singleInheritance/Dog.java
@@ -0,0 +1,8 @@
+package oop.inheritanceLab.singleInheritance;
+
+public class Dog extends Animal {
+
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
