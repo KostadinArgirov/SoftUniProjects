c3a69a61c79df2606789dd389135f8d71ab70a34
Kostadin <argirov.kostadin@gmail.com>
2022-07-11 18:27:24 +0300

    New Project Packages and Classes Added

---
+++ JavaAdvanced/src/oop/inheritanceExercises/animals/Tomcat.java
@@ -0,0 +1,11 @@
+package oop.inheritanceExercises.animals;
+
+public class Tomcat extends Cat {
+    public Tomcat(String name, int age) {
+        super(name, age, "Male");
+    }
+
+    public String produceSound() {
+        return "MEOW";
+    }
+}
