61e8ea50f481f7c083db484b79a89f9812517998
Kostadin <argirov.kostadin@gmail.com>
2022-07-12 12:23:17 +0300

    New Project Packages and Classes Added

---
+++ JavaAdvanced/src/oop/interfacesAndAbstractionLab/sayHello/European.java
@@ -0,0 +1,21 @@
+package oop.interfacesAndAbstractionLab.sayHello;
+
+public class European implements Person {
+
+    private String name;
+
+    public European(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+
+}
---
+++ JavaAdvanced/src/oop/interfacesAndAbstractionLab/sayHello/Person.java
@@ -0,0 +1,8 @@
+package oop.interfacesAndAbstractionLab.sayHello;
+
+public interface Person {
+
+    String getName();
+    String sayHello();
+
+}
