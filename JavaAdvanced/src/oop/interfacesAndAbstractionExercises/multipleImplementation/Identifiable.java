a6abac4a5aa2ce7854b25b0b80eb7f3583631776
Kostadin <argirov.kostadin@gmail.com>
2022-07-13 11:42:59 +0300

    New Project Packages and Classes Added

---
+++ JavaAdvanced/src/oop/interfacesAndAbstractionExercises/multipleImplementation/Identifiable.java
@@ -0,0 +1,7 @@
+package oop.interfacesAndAbstractionExercises.multipleImplementation;
+
+public interface Identifiable {
+
+    String getId();
+
+}
---
+++ JavaAdvanced/src/oop/interfacesAndAbstractionExercises/multipleImplementation/Person.java
@@ -0,0 +1,8 @@
+package oop.interfacesAndAbstractionExercises.multipleImplementation;
+
+public interface Person {
+
+    String getName();
+    int getAge();
+
+}
